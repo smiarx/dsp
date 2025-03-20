67ec2446c3584f50cffb39a65bdbff7b21083ac0
MS <maxime.sirbu@disroot.org>
2025-03-20 12:07:02 -0500

    add benchmarking springs

---
+++ bench/Springs.cpp
@@ -0,0 +1,22 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "../processors/springs/Springs.h"
+
+TEST_CASE("Springs")
+{
+    SECTION("Benchmark")
+    {
+        constexpr auto sampleRate = 48000.f;
+        constexpr auto blockSize  = 512;
+
+        float in[2][blockSize];
+        float *ins[2] = {in[0], in[1]};
+
+        processors::Springs springs;
+        springs.setSampleRate(sampleRate);
+        springs.update(0.5, 4500, 0.05, 2.3, 0.3, 0.4, 0.4, 1., 0.3, blockSize);
+
+        BENCHMARK("Springs") { springs.process(ins, ins, blockSize); };
+    }
+}
