f5a92e2aeecab57b4e02226f6db072b07936a3c9
MS <maxime.sirbu@disroot.org>
2025-08-02 06:18:12 -0500

    linalg, make vector a Nx1 matrix to generalize

--- include/dsp/LinearAlgebra.h
+++ include/dsp/LinearAlgebra.h
@@ -27,15 +27,19 @@ static constexpr auto kMatrixHeight   = MatrixInfos<M>::kHeight;
 template <class M> using MatrixBase_t = typename MatrixInfos<M>::type;
 
 ///////////////////////// getSIMD ///////////////////////////////////
-template <class V> inline auto getSIMD(const V &v, size_t k)
+template <class V> inline auto getSIMD(const V &v, size_t i, size_t j = 0)
 {
-    return v.getSIMD(k);
+    return v.getSIMD(i, j);
 }
-template <> inline auto getSIMD(const float &value, [[maybe_unused]] size_t k)
+template <>
+inline auto getSIMD(const float &value, [[maybe_unused]] size_t i,
+                    [[maybe_unused]] size_t j)
 {
     return value;
 }
-template <> inline auto getSIMD(const double &value, [[maybe_unused]] size_t k)
+template <>
+inline auto getSIMD(const double &value, [[maybe_unused]] size_t i,
+                    [[maybe_unused]] size_t j)
 {
     return value;
 }
@@ -48,26 +52,27 @@ enum class Op {
     kDiv,
 };
 
-template <Op op, typename X1, typename X2> class VecOp
+template <Op op, typename X1, typename X2> class MatOp
 {
-    static_assert((kMatrixHeight<X1> == kMatrixHeight<X2> ||
-                   kMatrixHeight<X1> == 1 || kMatrixHeight<X2> == 1) &&
-                  kMatrixWidth<X1> == 1 && kMatrixWidth<X2> == 1);
+    static_assert((kMatrixHeight<X1> == kMatrixHeight<X2> &&
+                   kMatrixWidth<X1> == kMatrixWidth<X2>) ||
+                  (kMatrixHeight<X1> == 1 && kMatrixWidth<X1> == 1) ||
+                  (kMatrixHeight<X2> == 1 && kMatrixWidth<X2> == 1));
 
   public:
-    VecOp(const X1 &x1, const X2 &x2) : x1_(x1), x2_(x2) {}
+    MatOp(const X1 &x1, const X2 &x2) : x1_(x1), x2_(x2) {}
 
-    auto getSIMD(size_t k) const
+    auto getSIMD(size_t i, size_t j = 0) const
     {
         namespace itrn = internal;
         if constexpr (op == Op::kAdd) {
-            return itrn::getSIMD(x1_, k) + itrn::getSIMD(x2_, k);
+            return itrn::getSIMD(x1_, i, j) + itrn::getSIMD(x2_, i, j);
         } else if constexpr (op == Op::kSub) {
-            return itrn::getSIMD(x1_, k) - itrn::getSIMD(x2_, k);
+            return itrn::getSIMD(x1_, i, j) - itrn::getSIMD(x2_, i, j);
         } else if constexpr (op == Op::kMul) {
-            return itrn::getSIMD(x1_, k) * itrn::getSIMD(x2_, k);
+            return itrn::getSIMD(x1_, i, j) * itrn::getSIMD(x2_, i, j);
         } else if constexpr (op == Op::kDiv) {
-            return itrn::getSIMD(x1_, k) / itrn::getSIMD(x2_, k);
+            return itrn::getSIMD(x1_, i, j) / itrn::getSIMD(x2_, i, j);
         } else {
             static_assert(false, "Invalid operator");
         }
@@ -78,193 +83,60 @@ template <Op op, typename X1, typename X2> class VecOp
     const X2 &x2_;
 };
 template <Op op, typename X1, typename X2>
-struct MatrixInfos<VecOp<op, X1, X2>> {
+struct MatrixInfos<MatOp<op, X1, X2>> {
     static constexpr auto kHeight = kMatrixHeight<X1> > kMatrixHeight<X2>
                                         ? kMatrixHeight<X1>
                                         : kMatrixHeight<X2>;
-    static constexpr auto kWidth  = 1;
+    static constexpr auto kWidth  = kMatrixWidth<X1> > kMatrixWidth<X2>
+                                        ? kMatrixWidth<X1>
+                                        : kMatrixWidth<X2>;
     using type                    = MatrixBase_t<X1>;
 };
 
-///////////////////////// AbstractVector ///////////////////////////////////
-template <class Vec> class AbstractVector
+///////////////////////// AbstractMatrix ///////////////////////////////////
+template <class Mat> class AbstractMatrix
 {
-    static_assert(kMatrixWidth<Vec> == 1);
-
   public:
-    AbstractVector(Vec &&vec) : v_(vec) {}
+    AbstractMatrix(Mat &&mat) : mat_(mat) {}
 
-    inline auto getSIMD(size_t k) const { return internal::getSIMD(v_, k); }
-
-    template <class V> auto operator+(const V &other) const
+    always_inline auto getSIMD(size_t i, size_t j = 0) const
     {
-        using namespace internal;
-        auto op = VecOp<Op::kAdd, AbstractVector, V>(*this, other);
-        return AbstractVector(std::move(op));
+        return internal::getSIMD(mat_, i, j);
     }
 
-    template <class V> auto operator-(const V &other) const
+    template <class M> auto operator+(const M &other) const
     {
         using namespace internal;
-        auto op = VecOp<Op::kSub, AbstractVector, V>(*this, other);
-        return AbstractVector(std::move(op));
+        auto op = MatOp<Op::kAdd, AbstractMatrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
     }
 
-    template <class V> auto operator*(const V &other) const
+    template <class M> auto operator-(const M &other) const
     {
         using namespace internal;
-        auto op = VecOp<Op::kMul, AbstractVector, V>(*this, other);
-        return AbstractVector(std::move(op));
+        auto op = MatOp<Op::kSub, AbstractMatrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
     }
 
-    template <class V> auto operator/(const V &other) const
+    template <class M> auto operator*(const M &other) const
     {
         using namespace internal;
-        auto op = VecOp<Op::kDiv, AbstractVector, V>(*this, other);
-        return AbstractVector(std::move(op));
+        auto op = MatOp<Op::kMul, AbstractMatrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
+    }
+
+    template <class M> auto operator/(const M &other) const
+    {
+        using namespace internal;
+        auto op = MatOp<Op::kDiv, AbstractMatrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
     }
 
   private:
-    const Vec v_;
+    const Mat mat_;
 };
 
 } // namespace internal
-  //
-template <typename T, size_t H, size_t W> class Matrix;
-
-///////////////////////// Vector ///////////////////////////////////
-template <typename T, size_t N> class Vector
-{
-    // Vector is store as simd batches,
-    // we pad zeros in the front
-    // e.g. vector of size 9
-    //  {{0 0 a0 a1} {a2 a3 a4} {a5 a6 a7 a8}}
-    //
-
-  public:
-    static constexpr auto kSIMDSize =
-        std::min(DSP_MAX_VEC_SIZE, static_cast<int>(nextPow2(sizeof(T) * N))) /
-        sizeof(T);
-    static constexpr auto kN     = nextAlignedOffset(N, kSIMDSize);
-    static constexpr auto kNsimd = kN / kSIMDSize;
-
-    Vector() = default;
-    Vector(const T *data)
-    {
-        // unaligned
-        size_t k = 0;
-        if constexpr (kNsimd != kN) {
-            for (size_t i = 0; i < kN - N; ++i) {
-                data_[k][i] = 0;
-            }
-            for (size_t i = kN - N; i < kSIMDSize; ++i) {
-                data_[k][i] = *data;
-                ++data;
-            }
-            ++k;
-        }
-
-        for (; k < kNsimd; ++k) {
-            data_[k] = batch<T>::loadu(data);
-            data += kSIMDSize;
-        }
-    }
-
-    T operator[](size_t i)
-    {
-        i += kN - N;
-        auto k = i / kSIMDSize;
-        i      = i % kSIMDSize;
-        return data_[k][i];
-    }
-
-    [[nodiscard]] auto getSIMD(size_t k) const
-    {
-        assert(k < kNsimd);
-        return data_[k].load();
-    }
-
-    template <class V> Vector &operator=(const V &vector)
-    {
-        store(vector);
-        return *this;
-    }
-
-    template <class V, size_t K = 0> inline auto store(const V &vector)
-    {
-        for (size_t k = 0; k < kNsimd; ++k) data_[k].store(vector.getSIMD(k));
-    }
-
-    template <class V> inline auto dot(const V &vector)
-    {
-        T res{};
-        for (size_t k = 0; k < kNsimd; ++k)
-            res += sum(getSIMD(k) * (vector.getSIMD(k)));
-        return res;
-    }
-
-    template <class V> auto operator+(const V &other) const
-    {
-        using namespace internal;
-        auto op = VecOp<Op::kAdd, Vector, V>(*this, other);
-        return AbstractVector(std::move(op));
-    }
-
-    template <class V> auto operator-(const V &other) const
-    {
-        using namespace internal;
-        auto op = VecOp<Op::kSub, Vector, V>(*this, other);
-        return AbstractVector(std::move(op));
-    }
-
-    template <class V> auto operator*(const V &other) const
-    {
-        using namespace internal;
-        auto op = VecOp<Op::kMul, Vector, V>(*this, other);
-        return AbstractVector(std::move(op));
-    }
-
-    template <class V> auto operator/(const V &other) const
-    {
-        using namespace internal;
-        auto op = VecOp<Op::kDiv, Vector, V>(*this, other);
-        return AbstractVector(std::move(op));
-    }
-
-    template <size_t W> class Outer
-    {
-      public:
-        Outer(const Vector &v1, const Vector<T, W> &v2) : v1_(v1), v2_(v2) {}
-
-        [[nodiscard]] auto getSIMD(size_t i, size_t j) const
-        {
-            using Mat = Matrix<T, N, W>;
-            auto v1   = v1_.getSIMD(i);
-            j += Mat::kWOffset;
-            auto v2 = v2_.getSIMD(j / kSIMDSize);
-
-            return v1 * v2[j % kSIMDSize];
-        }
-
-      private:
-        const Vector &v1_;
-        const Vector<T, W> &v2_;
-    };
-
-    template <class V> auto outer(const V &other) const
-    {
-        return Outer<internal::kMatrixHeight<V>>(*this, other);
-    }
-
-  private:
-    std::array<multi<T, kSIMDSize>, kNsimd> data_{};
-};
-
-template <typename T, size_t N> struct internal::MatrixInfos<Vector<T, N>> {
-    static constexpr auto kHeight = N;
-    static constexpr auto kWidth  = 1;
-    using type                    = T;
-};
 
 ///////////////////////// Matrix ///////////////////////////////////
 template <typename T, size_t H, size_t W> class Matrix
@@ -273,15 +145,19 @@ template <typename T, size_t H, size_t W> class Matrix
     // we pad zeros in the front rows
     //
   public:
-    static constexpr auto kSIMDSize = Vector<T, H>::kSIMDSize;
-    static constexpr auto kHsimd    = Vector<T, H>::kNsimd;
-    static constexpr auto kNsimd    = kHsimd * W;
+    static constexpr auto kSIMDSize =
+        std::min(DSP_MAX_VEC_SIZE, static_cast<int>(nextPow2(sizeof(T) * H))) /
+        sizeof(T);
+    static constexpr auto kH       = nextAlignedOffset(H, kSIMDSize);
+    static constexpr auto kSubMatH = kH / kSIMDSize;
 
-    static constexpr auto kSubMatH = kHsimd;
-    static constexpr auto kSubMatW = Vector<T, W>::kNsimd;
+    static constexpr auto kW       = nextAlignedOffset(W, kSIMDSize);
+    static constexpr auto kSubMatW = kW / kSIMDSize;
 
-    static constexpr auto kHOffset = Vector<T, H>::kN - H;
-    static constexpr auto kWOffset = Vector<T, W>::kN - W;
+    static constexpr auto kNsimd = kSubMatH * W;
+
+    static constexpr auto kHOffset = kH - H;
+    static constexpr auto kWOffset = kW - W;
 
     Matrix() = default;
     Matrix(const T *data)
@@ -324,38 +200,138 @@ template <typename T, size_t H, size_t W> class Matrix
 
     template <class V> auto mul(const V &v) const
     {
-        return internal::AbstractVector(MatMul(*this, v));
+        return internal::AbstractMatrix(MatMul(*this, v));
     }
 
-    class MatMul
+    template <size_t W2> class MatMul
     {
+        using Mat2 = Matrix<T, W, W2>;
+
       public:
-        MatMul(const Matrix &mat, const Vector<T, W> &vec) :
-            mat_(mat), vec_(vec)
+        MatMul(const Matrix &mat1, const Mat2 &mat2) : mat1_(mat1), mat2_(mat2)
         {
         }
 
-        auto getSIMD(size_t i) const
+        auto getSIMD(size_t i, size_t j = 0) const
         {
             simd<T, kSIMDSize> res{};
             for (size_t k = 0; k < kSubMatW; ++k) {
-                auto vec = vec_.getSIMD(k);
-                for (size_t j = k == 0 ? kWOffset : 0; j < kSIMDSize; ++j) {
+                auto vec = mat2_.getSIMD(k, j);
+                for (size_t l = k == 0 ? kWOffset : 0; l < kSIMDSize; ++l) {
                     auto matsimd =
-                        mat_.getSIMD(i, (k * kSIMDSize - kWOffset + j));
-                    res += vec[j] * matsimd;
+                        mat1_.getSIMD(i, (k * kSIMDSize - kWOffset + l));
+                    res += vec[l] * matsimd;
                 }
             }
             return res;
         }
 
       private:
-        const Matrix &mat_;
-        const Vector<T, W> &vec_;
+        const Matrix &mat1_;
+        const Mat2 &mat2_;
     };
 
+    template <class M> auto operator+(const M &other) const
+    {
+        using namespace internal;
+        auto op = MatOp<Op::kAdd, Matrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
+    }
+
+    template <class M> auto operator-(const M &other) const
+    {
+        using namespace internal;
+        auto op = MatOp<Op::kSub, Matrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
+    }
+
+    template <class M> auto operator*(const M &other) const
+    {
+        using namespace internal;
+        auto op = MatOp<Op::kMul, Matrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
+    }
+
+    template <class M> auto operator/(const M &other) const
+    {
+        using namespace internal;
+        auto op = MatOp<Op::kDiv, Matrix, M>(*this, other);
+        return AbstractMatrix(std::move(op));
+    }
+
   private:
-    std::array<std::array<multi<T, kSIMDSize>, W>, kHsimd> data_{};
+    std::array<std::array<multi<T, kSIMDSize>, W>, kSubMatH> data_{};
+};
+
+///////////////////////// Vector ///////////////////////////////////
+template <typename T, size_t N> class Vector : public Matrix<T, N, 1>
+{
+    // Vector is store as simd batches,
+    // we pad zeros in the front
+    // e.g. vector of size 9
+    //  {{0 0 a0 a1} {a2 a3 a4} {a5 a6 a7 a8}}
+    //
+
+  public:
+    using Mat                       = Matrix<T, N, 1>;
+    static constexpr auto kSIMDSize = Mat::kSIMDSize;
+    static constexpr auto kN        = Mat::kH;
+    static constexpr auto kNsimd    = Mat::kSubMatH;
+
+    Vector() = default;
+    Vector(const T *data) : Mat(data) {}
+
+    T operator[](size_t i) { return Mat::get(i, 0); }
+
+    [[nodiscard]] auto getSIMD(size_t i, size_t j = 0) const
+    {
+        return Mat::getSIMD(i, j);
+    }
+
+    template <class V> Vector &operator=(const V &vector)
+    {
+        Mat::store(vector);
+        return *this;
+    }
+
+    template <class V> inline auto dot(const V &vector)
+    {
+        T res{};
+        for (size_t k = 0; k < kNsimd; ++k)
+            res += sum(getSIMD(k) * (vector.getSIMD(k)));
+        return res;
+    }
+
+    template <size_t W> class Outer
+    {
+      public:
+        Outer(const Vector &v1, const Vector<T, W> &v2) : v1_(v1), v2_(v2) {}
+
+        [[nodiscard]] auto getSIMD(size_t i, size_t j) const
+        {
+            using Mat = Matrix<T, N, W>;
+            auto v1   = v1_.getSIMD(i);
+            j += Mat::kWOffset;
+            auto v2 = v2_.getSIMD(j / kSIMDSize);
+
+            return v1 * v2[j % kSIMDSize];
+        }
+
+      private:
+        const Vector &v1_;
+        const Vector<T, W> &v2_;
+    };
+
+    template <class V> auto outer(const V &other) const
+    {
+        return Outer<internal::kMatrixHeight<V>>(*this, other);
+    }
+};
+
+template <typename T, size_t N> struct internal::MatrixInfos<Vector<T, N>> {
+    static constexpr auto kHeight = N;
+    static constexpr auto kWidth  = 1;
+    using type                    = T;
 };
 
 template <typename T, size_t H, size_t W>
