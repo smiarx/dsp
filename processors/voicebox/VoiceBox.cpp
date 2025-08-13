a73bbea647ce39ddb5f40ab13ae0e762b11554bc
MS <maxime.sirbu@disroot.org>
2025-08-13 12:06:06 -0500

    add voicebox SC ugen

---
+++ processors/voicebox/VoiceBox.cpp
@@ -0,0 +1,54 @@
+#include "VoiceBox.h"
+
+namespace processors
+{
+inline namespace DSP_ARCH_NAMESPACE
+{
+
+void VoiceBox::update(float forgetFactor, float warpIn, float warpOut)
+{
+    rls_.setForgetFactor(forgetFactor);
+    filter_.setWarpIn(warpIn);
+    filter_.setWarpOut(warpOut);
+}
+
+void VoiceBox::process(const float *const *__restrict in,
+                       float *const *__restrict out, int count)
+{
+    const float *min = *in;
+    float *mout      = *out;
+
+    if (min != mout) {
+        for (int i = 0; i < count; ++i) mout[i] = min[i];
+    }
+
+    auto ctxt    = dsp::BufferContext(mout, count, buffer_);
+    auto ctxtDec = dsp::Context(mout);
+
+    decimate_.decimate(ctxt, ctxtDec, decimateDL_, decimateId_);
+    auto decBS = ctxtDec.getBlockSize();
+
+    CTXTRUN(ctxtDec)
+    {
+        auto origFilter = filter_;
+        rls_.process(ctxtDec, warpAState_, filter_);
+        filter_.compensateResidualAnalyze(ctxtDec, warpAState_);
+
+        filter_.compensateResidualReconstruct(ctxtDec, warpRState_);
+        origFilter.reconstruct(ctxtDec, warpRState_);
+    };
+
+    // move data to end of buffer to avoid write conflict
+    for (int i = 0; i < decBS; ++i) {
+        mout[count - i - 1] = mout[decBS - i - 1];
+    }
+    auto ctxtInt = dsp::BufferContext(&mout[count - decBS], decBS, bufferDec_);
+    decimateId_ =
+        interpolate_.interpolate(ctxtInt, ctxt, interpolateDL_, decimateId_);
+
+    buffer_.nextBlock(ctxt);
+    bufferDec_.nextBlock(ctxtDec);
+}
+
+} // namespace DSP_ARCH_NAMESPACE
+} // namespace processors
